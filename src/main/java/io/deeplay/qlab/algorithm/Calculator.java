7d3ebdeb81bfe3616d9e34cc557dfd642109dce3
Sergey Romanov <romanovomsk2014@gmail.com>
2022-04-12 13:32:07 +0600

    переделал калькулятор

--- src/main/java/io/deeplay/qlab/algorithm/Calculator.java
+++ src/main/java/io/deeplay/qlab/algorithm/Calculator.java
@@ -1,19 +1,19 @@
 package io.deeplay.qlab.algorithm;
 
 import io.deeplay.qlab.parser.models.Unit;
-import io.deeplay.qlab.parser.models.history.Round;
+import io.deeplay.qlab.parser.models.input.EnemyLocation;
+import io.deeplay.qlab.parser.models.output.UnitWithLocation;
 
 import java.util.List;
 import java.util.Random;
 
 public class Calculator {
 
-    public static void calculate(Unit unit, List<Round> rounds) {
+    public static UnitWithLocation calculate(Unit unit, List<EnemyLocation> locations) {
         Random rand = new Random();
-        unit.setAggression(rand.nextInt(100));
-        unit.setEvasiveness(rand.nextInt(100));
-        unit.setResponseAggression(rand.nextInt(100));
-        unit.setShield(rand.nextInt(100));
-    }
+        EnemyLocation location = locations.get(rand.nextInt(locations.size()));
+        int locatePosition = rand.nextInt(location.getMaxPositionsQuantity());
 
+        return new UnitWithLocation(unit.getName(), unit.getSourceGoldCount(), locatePosition, location.getLocationName());
+    }
 }
--- src/main/java/io/deeplay/qlab/algorithm/MegaFinder.java
+++ src/main/java/io/deeplay/qlab/algorithm/MegaFinder.java
@@ -1,9 +1,14 @@
 package io.deeplay.qlab.algorithm;
 
+import io.deeplay.qlab.parser.models.Unit;
+import io.deeplay.qlab.parser.models.input.InputData;
+
+import java.util.List;
+
 public class MegaFinder {
 
-    /*public List<Unit> findOptDisposition(World world) {
+    public List<Unit> findOptDisposition(InputData world) {
         return world.getOurUnits();
-    }*/
+    }
 
 }
