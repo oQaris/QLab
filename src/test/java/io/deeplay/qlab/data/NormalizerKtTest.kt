a0a78470b2d5fb2fc8812534bcf680ea7eb6629a
0razzu <0razzu@protonmail.com>
2022-06-14 01:43:18 +0600

    NormalizerKtTest: float instead of double, eps

--- src/test/java/io/deeplay/qlab/data/NormalizerKtTest.kt
+++ src/test/java/io/deeplay/qlab/data/NormalizerKtTest.kt
@@ -5,15 +5,16 @@ import org.junit.jupiter.api.Test
 
 internal class NormalizerKtTest {
 
+    private val eps = 1E-6f
     private val data = listOf(0, -1, 2, 8, 1, 1, 4, -6, 0)
 
     @Test
     fun meanTest() {
-        assertEquals(1.0, data.mean { it.toFloat() })
+        assertEquals(1.0f, data.mean { it.toFloat() }, eps)
     }
 
     @Test
     fun standardDeviationTest() {
-        assertEquals(3.559026084010437, data.standardDeviation { it.toFloat() })
+        assertEquals(3.559026084010437f, data.standardDeviation { it.toFloat() }, eps)
     }
 }
