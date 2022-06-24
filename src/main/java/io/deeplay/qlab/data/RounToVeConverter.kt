1ada8e409ca72dff75f56f88865b97fa055d1ca4
Sergey Romanov <romanovomsk2014@gmail.com>
2022-06-24 20:54:59 +0600

    Обновление стандартизатора

--- src/main/java/io/deeplay/qlab/data/RounToVeConverter.kt
+++ src/main/java/io/deeplay/qlab/data/RounToVeConverter.kt
@@ -2,7 +2,6 @@ package io.deeplay.qlab.data
 
 import io.deeplay.qlab.parser.Parser
 import io.deeplay.qlab.parser.RoundListFilter
-import io.deeplay.qlab.parser.models.history.Round
 import java.io.File
 import java.util.*
 
@@ -13,9 +12,10 @@ fun main() {
     println("Всего данных: ${history.size}")
 
     println("Генерация профилей...")
-    val unitsProfiles = genUnitsProfiles(history)
-    val locationsProfiles = genLocationsProfiles(history)
-    val levels = history.map { it.locationLevel }.distinct().sorted()
+    val standardizer = Standardizer.fit(history)
+
+    println("Медианный профиль юнита: " + standardizer.medianUnitProfile)
+    println("Медианный профиль локации: " + standardizer.medianLocationProfile)
 
     // Можно настроить выбор раундов
     val rounds = history.filter { it.ourUnits.size == 1 && it.opponentUnits.size == 1 }
@@ -29,81 +29,19 @@ fun main() {
     )
 
     println("Стандартизация данных...")
-    var roundsStd = standardizeRounds(rounds, levels, unitsProfiles, locationsProfiles)
+    var roundsStd = standardizer.transformAll(rounds, trimMaxPos = true)
 
     val normContext = genMinimaxNormContext(roundsStd)
     println("Контекст нормализации:")
     println(normContext.first.joinToString { String.format(Locale.ENGLISH, "%.8f", it) })
     println(normContext.second.joinToString { String.format(Locale.ENGLISH, "%.8f", it) })
 
-    //println("Нормализация данных...")
-    //roundsStd = normalizeStd(roundsStd) { normContext }
+    println("Нормализация данных...")
+    roundsStd = normalize(roundsStd) { normContext }
 
     println("Сохранение в файл...")
-    saveRounds(roundsStd, "trainData/11nnn.csv")
+    saveRounds(roundsStd, "trainData/11.csv")
     println("Сохранено ${roundsStd.size} ${roundsStd.first().size}-мерных векторов")
-}
-
-private fun standardizeRounds(
-    history: List<Round>,
-    levels: List<Int>,
-    unitsProfiles: Profiles,
-    locationsProfiles: Profiles
-): List<FloatArray> {
-
-    val sizeDataOneUnit = // Длина профиля юнита + дополнительные данные
-        unitsProfiles.entries.first().value.size + 2
-
-    val medianUnitProfile = transpose(unitsProfiles.values)
-        .map { col -> col.toList().median { it } }
-    println("Медианный профиль юнита: $medianUnitProfile")
-
-    val medianLocationProfile = transpose(locationsProfiles.values)
-        .map { col -> col.toList().median { it } }
-    println("Медианный профиль локации: $medianLocationProfile")
-
-    val maxPosition = history.maxOf { it.ourUnits.size + it.opponentUnits.size }
-    val roundsStandardized = mutableListOf<FloatArray>()
-
-    history.forEach { round ->
-        val curRoundNorm = mutableListOf<Float>()
-        // Профиль локации
-        curRoundNorm.addAll(
-            locationsProfiles[round.locationName]
-                ?.toList() ?: medianLocationProfile
-        )
-        // One-Hot encoding уровня
-        curRoundNorm.addAll(
-            FloatArray(levels.size)
-            { if (levels[it] == round.locationLevel) 1f else 0f }.toList()
-        )
-        // Данные о расположенных юнитах
-        val roundProfiles = (round.ourUnits + round.opponentUnits).let { units ->
-            buildList {
-                repeat(maxPosition) { pos ->
-                    val unit = units.firstOrNull { it.locatePosition == pos }
-                    if (unit != null) {
-                        val rawProfile = unitsProfiles[unit.name]
-                            ?.toList() ?: medianUnitProfile
-                        add(
-                            rawProfile // добавляем в конец sourceGold в раунде
-                                .plus((unit.sourceGoldCount).toFloat())
-                                .plus( // 1 - наш, 0 - не наш
-                                    if (unit in round.ourUnits) listOf(1f)
-                                    else listOf(0f)
-                                ).also { require(it.size == sizeDataOneUnit) }
-                        )
-                    } else add(FloatArray(sizeDataOneUnit).toList()) // нули
-                }
-            }
-        }.flatten()
-        curRoundNorm.addAll(roundProfiles)
-        // Голд профит
-        curRoundNorm.add((round.ourUnits.sumOf { it.goldProfit }).toFloat())
-
-        roundsStandardized.add(curRoundNorm.toFloatArray())
-    }
-    return roundsStandardized
 }
 
 private fun normalize(
@@ -113,22 +51,15 @@ private fun normalize(
     return data.map { it.applyNormByRow(normContext(data)).toFloatArray() }
 }
 
-private fun transpose(src: Collection<FloatArray>) = buildList {
-    for (nCol in src.first().indices) {
-        val column = src.map { it[nCol] }
-        add(column.toFloatArray())
-    }
-}
-
 private fun saveRounds(rounds: List<FloatArray>, fileName: String) {
     File(fileName)
         .also { it.parentFile.mkdirs() }
         .bufferedWriter().use { writer ->
-            /*repeat(rounds.first().size - 1) {
+            repeat(rounds.first().size - 1) {
                 writer.write("p${it},")
-            }*/
+            }
             // Для 1 на 1 будет такой порядок:
-            writer.write("evl,agl,ral,shl,sgl,gpl,vrl,lvl1,lvl2,lvl3,lvl4,lvl5,lvl6,lvl7,lvl8,lvl9,lvl10,ev1,ag1,ra1,sh1,sg1,gp1,vr1,sgc1,our1,ev2,ag2,ra2,sh2,sg2,gp2,vr2,sgc2,our2,")
+            //writer.write("evl,agl,ral,shl,sgl,gpl,vrl,lvl1,lvl2,lvl3,lvl4,lvl5,lvl6,lvl7,lvl8,lvl9,lvl10,ev1,ag1,ra1,sh1,sg1,gp1,vr1,sgc1,our1,ev2,ag2,ra2,sh2,sg2,gp2,vr2,sgc2,our2,")
             writer.write("our_gp")
             writer.newLine()
 
---
+++ src/main/java/io/deeplay/qlab/data/Standardizer.kt
@@ -0,0 +1,95 @@
+package io.deeplay.qlab.data
+
+import io.deeplay.qlab.parser.models.history.Round
+import io.deeplay.qlab.parser.models.output.UnitWithLocation
+
+class Standardizer(
+    var maxPosition: Int,
+    val levels: List<Int>,
+    val unitsProfiles: Profiles,
+    val locationsProfiles: Profiles
+) {
+    val sizeDataOneUnit = // Длина профиля юнита + дополнительные данные
+        unitsProfiles.entries.first().value.size + 2
+
+    val medianUnitProfile = genMedianProfile(unitsProfiles)
+    val medianLocationProfile = genMedianProfile(locationsProfiles)
+
+    companion object {
+
+        fun fit(history: List<Round>): Standardizer {
+            val maxPosition = history.maxOf { it.ourUnits.size + it.opponentUnits.size }
+            val levels = history.map { it.locationLevel }.distinct().sorted()
+            val unitsProfiles = genUnitsProfiles(history)
+            val locationsProfiles = genLocationsProfiles(history)
+
+            return Standardizer(maxPosition, levels, unitsProfiles, locationsProfiles)
+        }
+    }
+
+    fun transformAll(rounds: List<Round>, trimMaxPos: Boolean = false): List<FloatArray> {
+        //TODO Костыль наверное
+        val oldMaxPos = maxPosition
+        if (trimMaxPos)
+            maxPosition = rounds.maxOf { it.ourUnits.size + it.opponentUnits.size }
+
+        return rounds.map { round ->
+            transform(round)
+        }.also {
+            maxPosition = oldMaxPos
+        }
+    }
+
+    fun transform(units: List<UnitWithLocation>): FloatArray {
+        TODO("Доделать")
+    }
+
+    fun transform(round: Round): FloatArray {
+        val curRoundNorm = mutableListOf<Float>()
+        // Профиль локации
+        curRoundNorm.addAll(
+            locationsProfiles[round.locationName]
+                ?.toList() ?: medianLocationProfile
+        )
+        // One-Hot encoding уровня
+        curRoundNorm.addAll(
+            FloatArray(levels.size)
+            { if (levels[it] == round.locationLevel) 1f else 0f }.toList()
+        )
+        // Данные о расположенных юнитах
+        val roundProfiles = (round.ourUnits + round.opponentUnits).let { units ->
+            buildList {
+                repeat(maxPosition) { pos ->
+                    val unit = units.firstOrNull { it.locatePosition == pos }
+                    if (unit != null) {
+                        val rawProfile = unitsProfiles[unit.name]
+                            ?.toList() ?: medianUnitProfile
+                        add(
+                            rawProfile // добавляем в конец sourceGold в раунде
+                                .plus((unit.sourceGoldCount).toFloat())
+                                .plus( // 1 - наш, 0 - не наш
+                                    if (unit in round.ourUnits) listOf(1f)
+                                    else listOf(0f)
+                                ).also { require(it.size == sizeDataOneUnit) }
+                        )
+                    } else add(FloatArray(sizeDataOneUnit).toList()) // нули
+                }
+            }
+        }.flatten()
+        curRoundNorm.addAll(roundProfiles)
+        // Голд профит
+        curRoundNorm.add((round.ourUnits.sumOf { it.goldProfit }).toFloat())
+        return curRoundNorm.toFloatArray()
+    }
+
+    private fun transpose(src: Collection<FloatArray>) = buildList {
+        for (nCol in src.first().indices) {
+            val column = src.map { it[nCol] }
+            add(column.toFloatArray())
+        }
+    }
+
+    private fun genMedianProfile(profiles: Profiles) =
+        transpose(profiles.values)
+            .map { col -> col.toList().median { it } }
+}
